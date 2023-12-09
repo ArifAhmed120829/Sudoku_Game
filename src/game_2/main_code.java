eb6b883d886ad90d585cd8eeba8661731049ceb2
ArifAhmed <arif15-12301@diu.edu.bd>
2023-12-09 18:17:11 +0600

    first_commit
    practice

---
+++ src/game_2/main_code.java
@@ -0,0 +1,10 @@
+package game_2;
+
+public class main_code {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ahmmm
+ *
+ */
+module Sudoku {
+}
