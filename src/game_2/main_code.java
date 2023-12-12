417d7fe30b775722053698b58cdc6503b0bd96a6
ArifAhmed <arif15-12301@diu.edu.bd>
2023-12-12 22:11:30 +0600

    updated
    completed

--- src/game_2/main_code.java
+++ src/game_2/main_code.java
@@ -2,25 +2,49 @@ package game_2;
 
 class Solution {
 	public void solveSudoku(char[][] board) {
-		
+		solve(board);
+		for(char[] c:board) System.out.println(c);
+	}
+
+	private boolean solve(char[][] board) {
+
 		int row = board.length;
 		int col = board[0].length;
 		for(int i = 0; i<row; i++) {
 			for(int j =0; j<col; j++) {
-				for(int val = 1; val<=9; val++) {
-					if(isSafe(i,j,board,val)) {
-						board[i][j] = (char)val;
+				if(board[i][j] == '.') {
+					for(int val = 1; val<=9; val++) {
+						if(isSafe(i,j,board,val)) {
+							board[i][j] = (char)('0'+val);
+							boolean ans = solve(board);
+							// basically after this line the value will choose between 1-9
+							//if after inputing number if somehow ans returns false meaning that
+							//somewhere in the code inputing that number returns false
+							//then it will try another value 
+							if(ans) return true;
+							
+							else board[i][j]= '.';//backtrack
+						}
 						
 					}
+					return false;
 				}
+				
 			}
 		}
-		for(char[] c:board) System.out.println(c);
+		return true;
+		
 	}
 
-	private boolean isSafe(int i, int j, char[][] board, int val) {
-		
-		return false;
+	private boolean isSafe(int r, int c, char[][] board, int val) {
+		//check row
+		char s = (char)('0'+val);
+		for(int i = 0; i<board.length; i++) {
+			if(board[r][i] == s) return false;
+			if(board[i][c] == s) return false;
+			if(board[3*(r/3) + (i/3)][3*(c/3)+(i%3)] == s) return false;
+		}
+		return true;
 	}
 }
 
